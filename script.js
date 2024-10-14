6338aaea07f16821d22ef5a6219fec6a0de876f2
Ali Naseri <alinasericodes@gmail.com>
2024-10-14 12:44:30 -0400

    Change layout

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
   }
-  
+
+
+// Function to toggle the visibility of the photo table
+document.getElementById('toggle-table').addEventListener('click', function() {
+  var table = document.getElementById('photo-table');
+  if (table.style.display === 'none') {
+      table.style.display = 'block';
+      this.textContent = 'Hide Photos'; // Change button text to 'Hide Photos'
+  } else {
+      table.style.display = 'none';
+      this.textContent = 'See me in actions'; // Change button text back to 'Show Photos'
+  }
+});
